c6f376bd00bb09efdf0dacf573f666730f7f606f
Denise Li <deniseli1231@gmail.com>
2013-10-23 11:33:21 -0500

    fixing possible capitalization errors

--- Reorder.py
+++ Reorder.py
@@ -6,16 +6,19 @@ END_PUNCTUATION = ['.', '!', '?']
 NO_SPACE_BEFORE_PUNCTUATION = ['\'', '\"', '\,', ':', ')', '/', '\\', ',']
 NO_SPACE_AFTER_PUNCTUATION = ['(' '/', '\\', '(']
 
-MIN_OUTPUT_LENGTH = 100
+MIN_OUTPUT_CHAR_LENGTH = 100
 
 def split_text():
     return nltk.word_tokenize(raw_text)
 
 def make_dict():
+    global dict_has_upper
     dict = {}
     for i in range(0, len(word_list) - 1):
         word = word_list[i]
         next = word_list[i + 1]
+        if word.istitle() or word.isdigit():
+            dict_has_upper = True
         if word in dict:
             if not next in dict[word]:
                 dict[word].append(next)
@@ -27,16 +30,25 @@ def print_hr():
     rows, columns = os.popen('stty size', 'r').read().split()
     print '-' * int(columns)
 
-def print_new_sentence():
-    # Choose first word
+def choose_first_word():
     word = random.choice(following_word_dict.keys())
-    while (word[0] < 'A' or word[0] > 'Z') and (word[0] < '0' or word[0] > '9'):
+    while not word.istitle() and not word.isdigit() and dict_has_upper:
         word = random.choice(following_word_dict.keys())
+    return word
+
+def print_new_segment():
+    # Choose first word
+    word = choose_first_word()
     sentence = word
     # Fill in more words until sentence completion.
     while not sentence[-1] in END_PUNCTUATION or \
-          len(sentence) < MIN_OUTPUT_LENGTH:
-        word = random.choice(following_word_dict[word])
+          len(sentence) < MIN_OUTPUT_CHAR_LENGTH:
+        if not word in following_word_dict:
+            word = choose_first_word()
+            if not sentence[-1] is '.':
+                sentence += "."
+        else:
+            word = random.choice(following_word_dict[word])
         if not word[0] in END_PUNCTUATION + NO_SPACE_BEFORE_PUNCTUATION and \
            not word[-1] in NO_SPACE_AFTER_PUNCTUATION:
             sentence += " "
@@ -45,7 +57,8 @@ def print_new_sentence():
 
 raw_text = raw_input("Enter the block of text you would like to reorder:\n> ")
 word_list = split_text()
+dict_has_upper = False
 following_word_dict = make_dict()
 print_hr()
-print_new_sentence()
+print_new_segment()
 print_hr()

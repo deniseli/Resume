93f2c58e97ffe1520c0dbb0893b20ce8ee16bc5c
Denise Li <deniseli1231@gmail.com>
2014-01-27 18:19:32 -0600

    paultag

--- Nathan.py
+++ Nathan.py
@@ -30,18 +30,25 @@ def nathan_is_a_pretty_big_deal(community):
                "Have you heard about my involvement with " + community + "?"]
     nathan_says(random.choice(sayings))
 
-def default_nathan():
-    if random.choice([1, 2]) is 1: talk_about_open_source()
-    else: talk_about_debian()
+def talk_about_paul():
+    sayings = ["Did you meet Paul Tagliamonte at R|P? He's @paultag on GitHub."]
+    nathan_says(random.choice(sayings))
 
+def default_nathan():
+    rand = random.randint(0, 2)
+    if rand is 0:   talk_about_open_source()
+    elif rand is 1: talk_about_debian()
+    elif rand is 2: talk_about_paul()
 
 
 input = ""
 nathan_says("Oh, hi!")
 while True:
-    input = raw_input("> ")
+    input = raw_input("> ").lower()
     if "fuck off, nathan" in input:
         nathan_says("Oh, come on!")
         sys.exit()
+    elif "paul tagliamonte" in input or "paultag" in input:
+        talk_about_paul()
     else:
         default_nathan()
